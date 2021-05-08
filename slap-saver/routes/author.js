3af58b14e0119903509f3076345fa19dd9251a6f
최준호 <choijunho@choejunhoui-MacBookAir.local>
2021-05-08 22:15:06 +0900

    feat: 페이지 간단한 마크업

--- slap-saver/routes/author.js
+++ slap-saver/routes/author.js
@@ -9,17 +9,25 @@ router.get('/', function(req, res, next) {
 
 // NOTE: 로그인 페이지
 router.get('/login', function(req, res, next) {
+  // NOTE: render: 무언가를 그려준다. context -> 상태
   res.render('author/login', { title: 'login!!!' });
 });
 
 // NOTE: 로그인 요청
-router.post('/login', (req, res) => {
+router.post('/login', function(req, res, next) {
+  // NOTE: 그냥 데이터를 클라이언트한테 보낸다. 그러면 브라우저가 자기가 알아서 클라이언트한테 보여준다. ft_write
   res.send(req.body);
 })
 
 // NOTE: 회원가입 페이지
 router.get('/signup', function(req, res, next) {
   res.render('author/signup', { title: 'signup!!!' });
+});
+
+
+// NOTE: 회원가입 요청
+router.post('/signup', function(req, res, next) {
+  res.send(req.body);
 });
 
 // NOTE: 편집회의 페이지
@@ -32,9 +40,13 @@ router.get('/articles/new', function(req, res, next) {
   res.render('author/newArticle', { title: '기사 작성 페이지!!' });
 });
 
+router.post('/articles/new', function(req, res, next) {
+  res.send(req.body);
+})
+
 // NOTE: 기사 작성 페이지(수정)
 router.get('/articles/edit', function(req, res, next) {
-  res.render('author/editArticle', { title: '기사 수정 페이지!!' });
+  res.send(req.body);
 });
 
 // NOTE: 내 기사목록 페이지
--- slap-saver/routes/index.js
+++ slap-saver/routes/index.js
@@ -3,28 +3,38 @@ var router = express.Router();
 
 /* GET home page. */
 // NOTE: 기본 홈 화면
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: '기본 홈 화면' });
 });
 
 // NOTE: 설정 화면 페이지
-router.get('/profile', function(req, res, next) {
+router.get('/profile', function (req, res, next) {
   res.render('index', { title: '설정 화면' });
 });
 
 // NOTE: 제보 화면 페이지
-router.get('/reporter', function(req, res, next) {
+router.get('/reporter', function (req, res, next) {
   res.render('index', { title: '제보 화면' });
 });
 
 // NOTE: 유저 로그인 페이지
-router.get('/login', function(req, res, next) {
+router.get('/login', function (req, res, next) {
   res.render('login', { title: '유저 로그인1!' });
 });
 
+// NOTE: 유저 로그인 요청
+router.post('/login', function (req, res, next) {
+  res.send(req.body);
+});
+
 // NOTE: 유저 회원가입 페이지
-router.get('/signup', function(req, res, next) {
+router.get('/signup', function (req, res, next) {
   res.render('signup', { title: '유저 회원가입!!' });
+});
+
+// NOTE: 유저 회원가입 요청
+router.post('/signup', function (req, res, next) {
+  res.send(req.body);
 });
 
 module.exports = router;
