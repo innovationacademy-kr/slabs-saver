00070c5b10d821dca769baec495b0476d604c154
sanam <simian114@gmail.com>
2021-05-10 17:47:29 +0900

    refactor: eslint 규칙에 맞게 파일 수정 var 를 const 루 수정했습니다. unamed-function 을 arrow-function 으로 수정했습니다.

--- slap-saver/routes/articles.js
+++ slap-saver/routes/articles.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
+const router = express.Router();
 /* GET home page. */
 
 // NOTE: 기사 메인 화면
-router.get('/:articleName', function(req, res, next) {
+router.get('/:articleName', (req, res, next) => {
   // NOTE: DB에서 articleId를 이용해서 기사를 빼온다
   res.render('articles/article', { title: req.params.articleId });
 });
