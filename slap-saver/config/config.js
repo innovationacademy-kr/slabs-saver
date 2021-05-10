4ba238936cc4d16fc181501f355ef0cb6bbbed29
sanam <simian114@gmail.com>
2021-05-10 19:41:43 +0900

    feat: sequelize 밑작업

---
+++ slap-saver/config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const development = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: 'mariadb',
+};
+
+const test = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: 'mariadb',
+};
+
+const production = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: 'mariadb',
+};
+
+module.exports = { development, production, test };
