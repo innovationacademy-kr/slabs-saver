de55df11bcdc59a12e023f165e87a3321a1acade
sanam <simian114@gmail.com>
2021-05-10 17:24:06 +0900

    feat: 서버 eslint 를 airbnb  로 세팅 eslint 의 기본 설정을 airbnb을 베이스로 수정했습니다. 따로 수정해준 rule 로는 line-break 이 있는데 윈도우와 unix 기반의 엔터의 포맷이 다르기 때문에 이 스타일은 무시하도록 만들었습니다

---
+++ slap-saver/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  rules: {
+    'linebreak-style': 0,
+  },
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+};
