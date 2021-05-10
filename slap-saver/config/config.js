ec6f73c4f5e49423536115ea15e08d519933acb1
sanam <simian114@gmail.com>
2021-05-10 23:16:30 +0900

    feat: db관련 환경변수 변경

--- slap-saver/config/config.js
+++ slap-saver/config/config.js
@@ -1,26 +1,26 @@
 require('dotenv').config();
 
 const development = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
   dialect: 'mariadb',
 };
 
 const test = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
   dialect: 'mariadb',
 };
 
 const production = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
   dialect: 'mariadb',
 };
 
