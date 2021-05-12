f07bf366b25ef007637688597b0f7e7b58c171c1
Sanghyeok Nam <sanam@c10r4s14.42seoul.kr>
2021-05-12 14:26:59 +0900

    feat: #3 로그아웃 기능 구현

--- slap-saver/controllers/authorController.js
+++ slap-saver/controllers/authorController.js
@@ -7,12 +7,25 @@ module.exports = {
   loginPage: (req, res, next) => {
     res.render('author/login', { title: 'login!!!' });
   },
-  login: (req, res, next) => {
-    res.send(req.body);
+
+  logout: (req, res, next) => {
+    if (req.user) {
+      console.log('---------------------------------------')
+      console.log('로그아웃 성공')
+      console.log('---------------------------------------')
+      req.logout();
+    } else {
+      console.log('-------- 로그인을 먼저 해주세요!! -----------')
+      console.log('로그인 한 상태가 아님')
+      console.log('---------------------------------------')
+    }
+    res.redirect('/author');
   },
+  
   signupPage: (req, res, next) => {
     res.render('author/signup', { title: 'author page' });
   },
+
   signup: async (req, res, next) => {
     const { email, password, confirm, contact } = req.body;
     const photo = req.file ? req.file.filename : null;
