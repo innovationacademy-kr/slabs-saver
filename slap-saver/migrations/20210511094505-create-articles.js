afb831dad5afaa477dfe303ae45107dc6b3f62bb
Kyunghan Min <kmin@c3r3s5.42seoul.kr>
2021-05-13 16:30:28 +0900

    fix: 내 기사목록 기사 수정하기 링크에러 수정
    내 기사목록에서 수정하기를 클릭한 경우 URL상의 '/'의 존재 유무에 따라
    에러가 발생하는 경우가 있음
    
    예를들면 /author/articles 는 수정하기를 클릭해도 에러가 없는 반면에
    /author/articles/에서는 에러가 발생함.
    
    원인은 수정하기에 href가 상대경로로 이동하는 문제가 있었기 때문이고,
    href를 절대 경로로 수정하여 문제를 해결하였다.

--- slap-saver/migrations/20210511094505-create-articles.js
+++ slap-saver/migrations/20210511094505-create-articles.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Articles', {
+    await queryInterface.createTable('Article', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -60,6 +60,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Articles');
+    await queryInterface.dropTable('Article');
   },
 };
