8d6ab410d5dbd1f85e22b6ef3674fe3b643562eb
Sanghyeok Nam <sanam@c3r3s7.42seoul.kr>
2021-05-13 19:53:11 +0900

    feat: #16 articles 와 author 모델 관계 형성

--- slap-saver/models/article.js
+++ slap-saver/models/article.js
@@ -7,7 +7,8 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Author }) {
+      this.belongsTo(Author);
       // define association here
     }
   }
@@ -61,10 +62,10 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.BOOLEAN,
         defaultValue: false,
       },
-      authorId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      }
+      // authorId: {
+      //   type: DataTypes.INTEGER,
+      //   allowNull: false,
+      // },
     },
     {
       sequelize,
--- slap-saver/models/author.js
+++ slap-saver/models/author.js
@@ -11,7 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Article }) {
+      this.hasMany(Article)
+
       // define association here
     }
 
