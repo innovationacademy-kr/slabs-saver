6b9784876c3e0cbd41ba31cf210adc949074756c
Kyunghan Min <kmin@c3r3s5.42seoul.kr>
2021-05-14 16:22:19 +0900

    refactor: #25 article 라우트 수정
    article의 라우트를 articleName에서 articleId로 수정하였습니다.

--- slap-saver/routes/articles.js
+++ slap-saver/routes/articles.js
@@ -3,6 +3,6 @@ const articleCtrl = require('../controllers/articleController');
 const router = express.Router();
 
 // NOTE: 기사 상세 화면
-router.get('/:articleName', articleCtrl.viewDetail);
+router.get('/:articleId', articleCtrl.viewDetail);
 
 module.exports = router;
