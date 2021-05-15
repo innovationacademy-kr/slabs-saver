c32345aeed1a96fa62d27894c647bc8207095735
Sanghyeok Nam <sanam@c3r5s7.42seoul.kr>
2021-05-15 16:08:57 +0900

    feat: #31 라이브러라 함수, 상수
    1. getCurrentUser 는 현재 유저를 식별하고, 리턴해줍니다.
    2. isEmptyObject 는 오브젝트가 비었는지 확인해줍니다.

---
+++ slap-saver/lib/getCurrentUser.js
@@ -0,0 +1,9 @@
+const { Author } = require('../models');
+
+module.exports = async (id) => {
+  const user = await Author.findOne({
+    where: { id },
+    attributes: ['desk'],
+  });
+  return user;
+}
