40d9903ebb5844962a4604b9d5a4658b99320e7c
kmin <kmin@student.42seoul.kr>
2021-05-16 14:13:33 +0900

    feat: #26 기자의 프로필 사진 추가
    기자의 사진 부분이 빠져있어서 추가하였다.

--- slap-saver/seeders/20210514003753-articles.js
+++ slap-saver/seeders/20210514003753-articles.js
@@ -4,23 +4,23 @@ const { Author } = require('../models');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const authors = await Author.findAll({});
-      await Promise.all(
-        authors.map(async (author) => {
-          await author.createArticle({
-            headline: `article from ${author.email}`,
-            category: 'politic',
-            author: author.email,
-            image: 'a74dc438fb6834f35e6c7968b57ae25f',
-            imageDesc: 'this is image description',
-            imageFrom: 'image source',
-            briefing: 'this is briefing',
-            additionalParagraph: 'additional paragraph',
-          })
-        })
-      );
+    await Promise.all(
+      authors.map(async (author) => {
+        await author.createArticle({
+          headline: `article from ${author.email}`,
+          category: 'politic',
+          author: author.email,
+          image: '97579e23841e5530661bcf43dc4001f3',
+          imageDesc: 'this is image description',
+          imageFrom: 'image source',
+          briefing: 'this is briefing',
+          additionalParagraph: 'additional paragraph',
+        });
+      }),
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Articles');
-  }
+  },
 };
