756ead3181e3b6096f29b8374c47fa47a77c9226
kmin <kmin@student.42seoul.kr>
2021-05-16 14:13:33 +0900

    feat: #26 기자의 프로필 사진 추가
    기자의 사진 부분이 빠져있어서 추가하였다.

--- slap-saver/controllers/userController.js
+++ slap-saver/controllers/userController.js
@@ -4,22 +4,28 @@ const moment = require('moment');
 module.exports = {
   home: async (req, res, next) => {
     // NOTE: where: { todayArticle: true } 인 것을 가져와야한다.
-    const candidateArticle = await Article.findOne({ where: { id: "1" } });
-    const todayArticle = candidateArticle ? candidateArticle : { headline: "비어있는 항목입니다."} ;
-    const todayWords = "helloworld";
-    const AtriclesObj = await Article.findAll({
-      where: { category: 'politic' }
-    });
-    const Articles = AtriclesObj.map((article) => {
-      const updatedAt = moment(article.updatedAt).format('YYYY.MM.DD HH:mm:ss');
-      const additionalParagraph = article.additionalParagraph ? article.additionalParagraph.split("|-|") : [];
-      return {...article.dataValues, 
-        image: `/images/articleImages/${article.image}`,
-        additionalParagraph,
-        updatedAt}
-    })
+    const candidateArticle = await Article.findOne({ where: { id: '1' } });
+    const todayArticle = candidateArticle ? candidateArticle : { headline: '비어있는 항목입니다.' };
+    const todayWords = 'helloworld';
+    const AtriclesObj = await Article.findAll({});
+    const Articles = await Promise.all(
+      AtriclesObj.map(async (article) => {
+        const updatedAt = moment(article.updatedAt).format('YYYY.MM.DD HH:mm:ss');
+        const additionalParagraph = article.additionalParagraph
+          ? article.additionalParagraph.split('|-|')
+          : [];
+        const { photo } = await article.getAuthor();
+        return {
+          ...article.dataValues,
+          authorImg: `/images/authorImages/${photo}`,
+          image: `/images/articleImages/${article.image}`,
+          additionalParagraph,
+          updatedAt,
+        };
+      }),
+    );
     // NOTE: 오늘의 한마디를 저장한 후 가져와야 한다.
-    res.render('index', { title: '기본 홈 화면', todayArticle, todayWords, Articles})
+    res.render('index', { title: '기본 홈 화면', todayArticle, todayWords, Articles });
   },
 
   profile: (req, res, next) => {
@@ -44,5 +50,5 @@ module.exports = {
 
   requestSignup: (req, res, next) => {
     res.send(req.body);
-  }
+  },
 };
