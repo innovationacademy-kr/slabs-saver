bc803bdcaecca4ed988b9efa38b610d9fe77dd49
sanam <simian114@gmail.com>
2021-05-16 18:52:36 +0900

    feat: #31 업데이트 시 서버에 정보 저장

--- slap-saver/seeders/20210514003753-articles.js
+++ slap-saver/seeders/20210514003753-articles.js
@@ -8,8 +8,36 @@ module.exports = {
     await Promise.all(
       authors.map(async (author) => {
         await author.createArticle({
-          headline: 'thisisheadline',
+          headline: '경제 기사',
+          category: CATEGORY.ECONOMY,
+          author: author.name,
+          image: 'a74dc438fb6834f35e6c7968b57ae25f',
+          imageDesc: 'this is image description',
+          imageFrom: 'image source',
+          briefing: 'this is briefing',
+          additionalParagraph: 'additional paragraph',
+        });
+      }),
+    );
+    await Promise.all(
+      authors.map(async (author) => {
+        await author.createArticle({
+          headline: '정치 기사',
           category: CATEGORY.POLITIC,
+          author: author.name,
+          image: 'a74dc438fb6834f35e6c7968b57ae25f',
+          imageDesc: 'this is image description',
+          imageFrom: 'image source',
+          briefing: 'this is briefing',
+          additionalParagraph: 'additional paragraph',
+        });
+      }),
+    );
+    await Promise.all(
+      authors.map(async (author) => {
+        await author.createArticle({
+          headline: '스포츠 기사',
+          category: CATEGORY.SPORTS,
           author: author.name,
           image: 'a74dc438fb6834f35e6c7968b57ae25f',
           imageDesc: 'this is image description',
