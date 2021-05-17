1747d4f3dfb8f9035e9ecddfc792d9211d73d7b6
Sanghyeok Nam <sanam@c3r1s3.42seoul.kr>
2021-05-17 16:56:57 +0900

    feat: #39 #42 기자에 position 칼럼 추가

--- slap-saver/seeders/20210513063808-authors.js
+++ slap-saver/seeders/20210513063808-authors.js
@@ -8,6 +8,7 @@ module.exports = {
         name: 'desk',
         code: 2,
         desk: true,
+        position: 2,
         contact: '01012341234',
         password: '$2b$10$fO/O6fF5w1HDkXNab8AMBOYE/9ByW8/sjIeXpQONQgJxkegxdFDIq',
         photo: '0d948b18cc861228a65625ff96b5c4c7',
