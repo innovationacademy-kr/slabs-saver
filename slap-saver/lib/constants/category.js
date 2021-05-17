7c4f5a6f98e6256b9c283923459b8122b2154fab
sanam <simian114@gmail.com>
2021-05-18 00:12:12 +0900

    chore: 프리티어 포맷 적용

--- slap-saver/lib/constants/category.js
+++ slap-saver/lib/constants/category.js
@@ -1,10 +1,10 @@
 module.exports = {
-    ALL: [1, 2, 3, 4, 5, 6, 7],
-    POLITIC: 1,
-    ECONOMY: 2,
-    TECHNOLOGY: 3,
-    ENVIRONMENT: 4,
-    SCIENCE: 5,
-    HEALTH: 6,
-    SPORTS: 7,
-}
+  ALL: [1, 2, 3, 4, 5, 6, 7],
+  POLITIC: 1,
+  ECONOMY: 2,
+  TECHNOLOGY: 3,
+  ENVIRONMENT: 4,
+  SCIENCE: 5,
+  HEALTH: 6,
+  SPORTS: 7,
+};
