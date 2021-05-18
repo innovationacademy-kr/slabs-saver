56614f56e2f79d5addcead099f0a7672d598cd01
Sanghyeok Nam <sanam@c3r3s7.42seoul.kr>
2021-05-18 19:16:05 +0900

    feat: #28 paragraphs 컬럼 추가 및 기존 additionalParagraph 삭제

--- slap-saver/models/article.js
+++ slap-saver/models/article.js
@@ -45,11 +45,6 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.TEXT,
         allowNull: false,
       },
-      additionalParagraph: {
-        type: DataTypes.TEXT,
-      },
-      // NOTE: 출고 여부를 알려준다. 출고가 됐다는 것은 편집장의 최종 승인이 있었다는 것.
-      // NOTE: am7, pm7 출고가 된 기사들만 체크를 할 수 있게 만들어야겠지?
       am7: {
         type: DataTypes.BOOLEAN,
         defaultValue: false,
@@ -76,6 +71,9 @@ module.exports = (sequelize, DataTypes) => {
         get() {
           return moment(this.getDataValue('publishedAt')).format('YYYY.MM.DD HH:mm:ss');
         },
+      },
+      paragraphs: {
+        type: DataTypes.JSON,
       },
       createdAt: {
         allowNull: false,
