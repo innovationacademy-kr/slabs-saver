e611fd23c5a5a44b38f27876a0fa2571c16623d5
Kyunghan Min <kmin@c3r3s5.42seoul.kr>
2021-05-18 18:27:05 +0900

    feat: #43 관리자의 가입 승인과정 구현
    관리자가 언론사의 새로운 사원에게 가입을 허용할지
    거부할지를 결정합니다.
    
    관리자 페이지에서는 가입 대기, 가입 승인,
    가입 완료, 거절의 4가지 단계를  확인할 수 있습니다.

---
+++ slap-saver/lib/sendMail.js
@@ -0,0 +1,31 @@
+const nodemailer = require('nodemailer');
+
+module.exports = (email, name, code) => {
+  if (code != "0") {
+    const myEmail = {
+      host: process.env.MAILTRAP_HOST,
+      port: 2525,
+      secure: false,
+      auth: {
+        user: process.env.MAILTRAP_USER_NAME,
+        pass: process.env.MAILTRAP_PASSWORD,
+      },
+    };
+    const send = async (data) => {
+      nodemailer.createTransport(myEmail).sendMail(data, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          return info.response;
+        }
+      });
+    };
+    const content = {
+      from: 'saver@saver.com',
+      to: email,
+      subject: 'saver입니다',    
+      html: `<h1>SAVER</h1><a href="">http://localhost:4000/author/signup?email=${email}&name=${name}&code=${code}</a>`,
+    };
+    send(content);
+  }
+}
