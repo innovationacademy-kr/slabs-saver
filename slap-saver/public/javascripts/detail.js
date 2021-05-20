17504923413b7c3d2942ceae0a2c0f05cc775181
Kyunghan Min <kmin@c3r3s5.42seoul.kr>
2021-05-20 18:44:27 +0900

    feat: #53 모달 구현
    관리자가 직접 기자를 초대하기 위한
    모달기능을 구현하였습니다.

--- slap-saver/public/javascripts/detail.js
+++ slap-saver/public/javascripts/detail.js
@@ -2,6 +2,20 @@ var detailBtn = document.querySelector('.detail--button');
 
 var detailSection = document.querySelector('.detail-sections');
 
+var closeBtn = document.querySelector('.closeBtn');
+
+var inviteSection = document.querySelector('.invite-section');
+
 detailBtn.addEventListener('click', () => {
   detailSection.style.display = detailSection.style.display === 'none' ? 'block' : 'none';
 });
+
+closeBtn.addEventListener('click', () => {
+  inviteSection.style.display = "none";
+})
+
+window.onclick = function(event) {
+  if (event.target == inviteSection) {
+    inviteSection.style.display = "none";
+  }
+}
