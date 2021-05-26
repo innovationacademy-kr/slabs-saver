0487cb37cffe05edb1e115febc3ac13bc8119930
Kyunghan Min <kmin@c3r3s5.42seoul.kr>
2021-05-26 13:42:35 +0900

    chore: #82 자세히보기 버튼 수정 자세히보기 버튼은 한 번 누르면 사라지도록 변경하였습니다.

--- slap-saver/public/javascripts/detail.js
+++ slap-saver/public/javascripts/detail.js
@@ -3,6 +3,7 @@ var detailBtn = document.querySelector('.detail--button');
 var detailSection = document.querySelector('.detail-sections');
 
 detailBtn.addEventListener('click', () => {
-  detailSection.style.display = detailSection.style.display === 'none' ? 'block' : 'none';
+  detailSection.style.display = 'block';
+  detailBtn.style.display = "none";
 });
 
