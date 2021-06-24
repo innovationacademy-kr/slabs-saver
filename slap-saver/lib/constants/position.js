ca1ecd93880b812b90dc811e1977215df2be7958
Yenowme <76904042+Yenowme@users.noreply.github.com>
2021-06-24 19:16:59 +0900

    #125 #126 외부필진, 인턴 회원가입 로직에 추가

--- slap-saver/lib/constants/position.js
+++ slap-saver/lib/constants/position.js
@@ -2,10 +2,14 @@ const REPOTER = 1;
 const DESK = 2;
 const CHIEF_EDITOR = 3;
 const ADMIN = 4;
+const INTERN = 5;
+const WRITTER = 6;
 
 module.exports = {
 	REPOTER,
 	DESK,
 	CHIEF_EDITOR,
-	ADMIN
+	ADMIN,
+	INTERN,
+	WRITTER
 }
