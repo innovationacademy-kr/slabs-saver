5ac447da3ef1e567ce630bf09fa4fdade800cce4
Yenowme <76904042+Yenowme@users.noreply.github.com>
2021-06-29 15:24:31 +0900

    #151 오늘의 한마디 db테이블 구성

---
+++ slap-saver/migrations/20210628064146-create-today-words.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('TodayWords', {
+
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      word: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      AuthorId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'cascade',
+        references: {
+          model: 'Authors',
+          key: 'id',
+        },
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('TodayWords');
+  }
+};
