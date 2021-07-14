61f1e775dee556f4c38f2f0e02cdbe8f4a57da68
feldblume5263 <feldblume5263@gmail.com>
2021-07-15 00:14:29 +0900

    init iOS project

---
+++ saver-ios/Pods/Target Support Files/Pods-saver-ios-saver-iosUITests/Pods-saver-ios-saver-iosUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_saver_ios_saver_iosUITests : NSObject
+@end
+@implementation PodsDummy_Pods_saver_ios_saver_iosUITests
+@end
---
+++ saver-ios/saver-ios/ViewController.swift
@@ -0,0 +1,63 @@
+import UIKit
+import WebKit
+
+class ViewController: UIViewController,WKUIDelegate,WKNavigationDelegate {
+    
+    
+    @IBOutlet var webView: WKWebView!
+    
+    
+    override func loadView() {
+        super.loadView()
+        
+        webView = WKWebView(frame: self.view.frame)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        self.view = self.webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "http://localhost:1234")
+        let request = URLRequest(url: url!)
+        self.webView?.allowsBackForwardNavigationGestures = true  //뒤로가기 제스쳐 허용
+//        webView.configuration.preferences.javaScriptEnabled = true  //자바스크립트 활성화
+        webView.load(request)
+    }
+    
+    //모달창 닫힐때 앱 종료현상 방지.
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+    }
+    
+    //alert 처리
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String,
+                 initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in completionHandler() }))
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    //confirm 처리
+    func webView (_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "취소", style: .default, handler: { (action) in completionHandler(false) }))
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in completionHandler(true) }))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    // href="_blank" 처리
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        
+        if navigationAction.targetFrame == nil { webView.load(navigationAction.request) }
+        return nil
+        
+    }
+    
+}
