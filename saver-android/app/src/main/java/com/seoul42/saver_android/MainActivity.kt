ea2f4275a997243e4aa703b7cfa1e4eb41982b5f
hokim <teamgoodaloe@gmail.com>
2021-07-15 16:34:01 +0900

    [feat]: android 기본 환경 세팅 - android studio 기본 환경 세팅 - minimum sdk는  Android 6.0 Marshmallow 으로 설정했습니다.

---
+++ saver-android/app/src/main/java/com/seoul42/saver_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.seoul42.saver_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
