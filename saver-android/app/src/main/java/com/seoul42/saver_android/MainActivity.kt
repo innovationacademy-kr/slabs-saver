9b2d44d19c08d5cb4fcaa02eda2f1bf4444c4880
junhypar <junhypar@student.42seoul.kr>
2021-07-15 17:12:52 +0900

    [feat]: #210 android webView적용 -'https://thesaver.io' webView적용

--- saver-android/app/src/main/java/com/seoul42/saver_android/MainActivity.kt
+++ saver-android/app/src/main/java/com/seoul42/saver_android/MainActivity.kt
@@ -2,10 +2,33 @@ package com.seoul42.saver_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.main_webView)
+
+        //apply를 통해 myWebView.~~~ 의세팅을 블럭으로 해결
+        myWebView.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+        }
+        myWebView.loadUrl("https://thesaver.io")
+    }
+
+    override fun onBackPressed() {
+
+        val myWebView: WebView = findViewById(R.id.main_webView)
+
+        if (myWebView.canGoBack()) {
+            myWebView.goBack()
+        }
+        else {
+            finish()
+        }
     }
 }
