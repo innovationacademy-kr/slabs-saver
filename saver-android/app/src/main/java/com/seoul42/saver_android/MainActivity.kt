e1fe4f1fa3182cb5e7a3822ba96543d5c93033b4
hokim <teamgoodaloe@gmail.com>
2021-07-19 18:12:21 +0900

    feat: #226 파이어베이스와 안드로이드 연동 -파이어베이스와 안드로이드 연동 -구독 후 해당 토픽으로 메세지를 보내면 onMessageReceived에서 응답

--- saver-android/app/src/main/java/com/seoul42/saver_android/MainActivity.kt
+++ saver-android/app/src/main/java/com/seoul42/saver_android/MainActivity.kt
@@ -3,12 +3,15 @@ package com.seoul42.saver_android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
 
 class MainActivity : AppCompatActivity() {
 
-   private val myWebView: WebView by lazy{
+    private val myWebView: WebView by lazy {
         findViewById(R.id.main_webView)
     }
 
@@ -21,19 +24,44 @@ class MainActivity : AppCompatActivity() {
 
         myWebView.apply {
             webViewClient = WebViewClient()
-            settings.domStorageEnabled =true
+            settings.domStorageEnabled = true
             settings.javaScriptEnabled = true
         }
+
         myWebView.loadUrl("https://thesaver.io")
     }
 
     override fun onBackPressed() {
         if (myWebView.canGoBack()) {
             myWebView.goBack()
-        }
-        else {
+        } else {
             finish()
         }
+    }
+
+    private fun subscribeTopic(topic: String) {
+        Firebase.messaging.subscribeToTopic(topic)
+            .addOnCompleteListener { task ->
+                var msg = ""
+                msg = if (task.isSuccessful)
+                    "성공"
+                else
+                    "실패"
+                Log.d("구독", msg)
+            }
+
+    }
+
+    private fun unsubscribeTopic(topic: String) {
+        Firebase.messaging.unsubscribeFromTopic(topic)
+            .addOnCompleteListener { task ->
+                var msg = "성공"
+                if (!task.isSuccessful) {
+                    msg = "실패"
+                }
+                Log.d("구독 취소", msg)
+            }
+
     }
 
 }
