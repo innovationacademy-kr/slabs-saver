5c3a40914888aabec96babbff6e80721ddb17502
feldblume5263 <feldblume5263@gmail.com>
2021-07-21 15:00:24 +0900

    feat: #229 launch screen layout, image 수정 + auto layout 적용(가로화면, 여러 크기의 화면 대응). + 앱 실행 시, 1초 delay 적용. + image 수정.
    > copyright 글씨체 설치 후 적용 필요.

--- saver-ios/saver-ios/AppDelegate.swift
+++ saver-ios/saver-ios/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        Thread.sleep(forTimeInterval: 1.0)
         return true
     }
 
