d916f3debb3914cb3f0164d0d849ab663c0e689a
hokim <teamgoodaloe@gmail.com>
2021-07-24 19:36:34 +0900

    feat: #253 공유버튼에 카카오톡 공유하기 기능 추가 - [/] 과 [/articles/detail/{id}] 위치에 있는 공유버튼에 카카오톡   공유하기 기능을 추가

--- saver-web/public/javascripts/infiniteScroll.js
+++ saver-web/public/javascripts/infiniteScroll.js
@@ -5,38 +5,79 @@ var page = INITIAL_PAGE;
 var isUsed = false;
 var articleList = document.querySelector('.article-list');
 
+const KaKaoShare = (id, article) => {
+  if (!Kakao.isInitialized()) Kakao.init('카카오톡 JS 키');
+  const targetLink = `http://localhost:1234/articles/detail/${article.id}`;
+
+  Kakao.Link.createDefaultButton({
+    container: `#${id}`,
+    objectType: 'feed',
+    content: {
+      title: article.headline,
+      description: unescape(article.briefing),
+      imageUrl: article.image,
+      link: {
+        webUrl: targetLink,
+        mobileWebUrl: targetLink,
+        androidExecutionParams: 'test',
+      },
+    },
+    buttons: [
+      {
+        title: '웹으로 이동',
+        link: {
+          webUrl: targetLink,
+          mobileWebUrl: targetLink,
+        },
+      },
+      {
+        title: '앱으로 이동',
+        link: {
+          webUrl: targetLink,
+          mobileWebUrl: targetLink,
+        },
+      },
+    ],
+  });
+};
+
 const getPage = () => {
-	$.ajax({
-		url: `/moreArticles?page=${page}`,
-		type: 'get',
-		success: function (articles) {
-			articles = JSON.parse(articles);
-			articles.map(function (article) {
-				articleList.insertAdjacentHTML('beforeend', makeTemplate(article));
-			});
-			isUsed = false;
-			page += ADD_PAGE;
-		},
-		error: function (err) {
-		},
-	});
-}
+  $.ajax({
+    url: `/moreArticles?page=${page}`,
+    type: 'get',
+    success: function (articles) {
+      articles = JSON.parse(articles);
+      articles.map(function (article) {
+        articleList.insertAdjacentHTML('beforeend', makeTemplate(article));
+        const item = document.getElementById(`share-button_${article.id}`);
+        if (item) KaKaoShare(item.id, article);
+      });
+
+      // Array.from(articles).forEach((article) => {
+      // });
+
+      isUsed = false;
+      page += ADD_PAGE;
+    },
+    error: function (err) {},
+  });
+};
 
 //?계속 요청 보내는데??
 getPage();
 $(window).scroll(function () {
-	const scrollPosition = $(window).scrollTop() + DEFAULT_HEIGHT;
-	const pageHeight = $(document).height() - $(window).height();
+  const scrollPosition = $(window).scrollTop() + DEFAULT_HEIGHT;
+  const pageHeight = $(document).height() - $(window).height();
 
-	if (scrollPosition > pageHeight && !isUsed) {
-		isUsed = true;
-		getPage();
-	}
+  if (scrollPosition > pageHeight && !isUsed) {
+    isUsed = true;
+    getPage();
+  }
 });
 
 function makeTemplate(article) {
-	//추가되는 카드들
-	return `<div class="article">
+  //추가되는 카드들
+  return `<div class="article">
   <div class="article__top">
     <div class="icon-${article.category}-black"></div>
     <p class="article__top__text ft-detail">
@@ -61,7 +102,9 @@ function makeTemplate(article) {
       <div class="article__control__more-button__img"></div>
     </div>
     <div class="article__control__right-buttons">
-      <button class="article__control__right-buttons__share-button"></button>
+      <button class="article__control__right-buttons__share-button" id="share-button_${
+        article.id
+      }"></button>
       <button class="article__control__right-buttons__bookmark-button"></button>
     </div>
   </div>
--- saver-web/public/javascripts/user/article.js
+++ saver-web/public/javascripts/user/article.js
@@ -1,25 +1,66 @@
 class Editor {
+  constructor() {}
 
-	constructor() {
-	}
+  KaKaoShare = () => {
+    if (!Kakao.isInitialized()) Kakao.init('카카오톡 JS 키');
 
-	addContent = (selector, editorContent) => {
-		if ($(selector)) {
-			$(selector).append(editorContent)
-		} else {
-			throw '셀렉터를 확인해주세요';
-		}
-	}
+    const targetLink = document.location.href;
+    let headline = document.getElementsByClassName('article__title')[0].textContent;
+    headline = headline.trim();
+    let editor_briefing = document.getElementById('editor_briefing').textContent;
+    editor_briefing = editor_briefing.trim();
+    let image = document.getElementsByClassName('article__img')[0].src;
 
-	init = (briefingContent, paragraphsContent) => {
-		this.addContent('#editor_briefing', briefingContent)
-		this.addContent('#editor_paragraphs', paragraphsContent)
-	}
+    Kakao.Link.createDefaultButton({
+      container: `#btn_share`,
+      objectType: 'feed',
+      content: {
+        title: headline,
+        description: unescape(editor_briefing),
+        imageUrl: image,
+        link: {
+          webUrl: targetLink,
+          mobileWebUrl: targetLink,
+          androidExecutionParams: 'test',
+        },
+      },
+      buttons: [
+        {
+          title: '웹으로 이동',
+          link: {
+            webUrl: targetLink,
+            mobileWebUrl: targetLink,
+          },
+        },
+        {
+          title: '앱으로 이동',
+          link: {
+            webUrl: targetLink,
+            mobileWebUrl: targetLink,
+          },
+        },
+      ],
+    });
+  };
+
+  addContent = (selector, editorContent) => {
+    if ($(selector)) {
+      $(selector).append(editorContent);
+    } else {
+      throw '셀렉터를 확인해주세요';
+    }
+  };
+
+  init = (briefingContent, paragraphsContent) => {
+    this.addContent('#editor_briefing', briefingContent);
+    this.addContent('#editor_paragraphs', paragraphsContent);
+  };
 }
 
-
 $(document).ready(function () {
-	const editor = new Editor();
-	editor.init(unescape(briefingContent), unescape(paragraphsContent))
+  const editor = new Editor();
+  editor.init(unescape(briefingContent), unescape(paragraphsContent));
+  const btn = document.getElementById('btn_share');
+  if (btn) editor.KaKaoShare();
+  console.log(btn.id);
 });
-
