8b40f3cc36a3d535ab57eee7c1bd8d3449c8cf2b
Jisu <hellojs242@gmail.com>
2021-07-26 21:21:00 +0900

    feat: #253 카카오톡 공유하기 기능 모듈화
    - public/javascripts/totalShare.js로 분리.
    - 매개변수 통일을 위해 article.js에 객체 생성.

--- saver-web/public/javascripts/infiniteScroll.js
+++ saver-web/public/javascripts/infiniteScroll.js
@@ -5,42 +5,7 @@ var page = INITIAL_PAGE;
 var isUsed = false;
 var articleList = document.querySelector('.article-list');
 
-const KaKaoShare = (id, article) => {
-  if (!Kakao.isInitialized()) Kakao.init('카카오톡 JS 키');
-  const targetLink = `${document.location.href}articles/detail/${article.id}`;
-
-  Kakao.Link.createDefaultButton({
-    container: `#${id}`,
-    objectType: 'feed',
-    content: {
-      title: article.headline,
-      description: unescape(article.briefing),
-      imageUrl: article.image,
-      link: {
-        webUrl: targetLink,
-        mobileWebUrl: targetLink,
-        androidExecutionParams: 'test',
-      },
-    },
-    buttons: [
-      {
-        title: '웹으로 이동',
-        link: {
-          webUrl: targetLink,
-          mobileWebUrl: targetLink,
-        },
-      },
-      {
-        title: '앱으로 이동',
-        link: {
-          webUrl: targetLink,
-          mobileWebUrl: targetLink,
-          androidExecutionParams: `articles/detail/${article.id}`,
-        },
-      },
-    ],
-  });
-};
+import KaKaoShare from './totalShare.js';
 
 const getPage = () => {
   $.ajax({
@@ -51,11 +16,9 @@ const getPage = () => {
       articles.map(function (article) {
         articleList.insertAdjacentHTML('beforeend', makeTemplate(article));
         const item = document.getElementById(`share-button_${article.id}`);
+        article.path = `articles/detail/${article.id}`
         if (item) KaKaoShare(item.id, article);
       });
-
-      // Array.from(articles).forEach((article) => {
-      // });
 
       isUsed = false;
       page += ADD_PAGE;
--- saver-web/public/javascripts/user/article.js
+++ saver-web/public/javascripts/user/article.js
@@ -1,48 +1,6 @@
+import KaKaoShare from '../totalShare.js';
 class Editor {
   constructor() {}
-
-  KaKaoShare = () => {
-    if (!Kakao.isInitialized()) Kakao.init('카카오톡 JS 키');
-
-    const targetLink = document.location.href;
-    let headline = document.getElementsByClassName('article__title')[0].textContent;
-    headline = headline.trim();
-    let editor_briefing = document.getElementById('editor_briefing').textContent;
-    editor_briefing = editor_briefing.trim();
-    let image = document.getElementsByClassName('article__img')[0].src;
-
-    Kakao.Link.createDefaultButton({
-      container: `#btn_share`,
-      objectType: 'feed',
-      content: {
-        title: headline,
-        description: unescape(editor_briefing),
-        imageUrl: image,
-        link: {
-          webUrl: targetLink,
-          mobileWebUrl: targetLink,
-          androidExecutionParams: 'test',
-        },
-      },
-      buttons: [
-        {
-          title: '웹으로 이동',
-          link: {
-            webUrl: targetLink,
-            mobileWebUrl: targetLink,
-          },
-        },
-        {
-          title: '앱으로 이동',
-          link: {
-            webUrl: targetLink,
-            mobileWebUrl: targetLink,
-            androidExecutionParams: `articles/detail/${article.id}`,
-          },
-        },
-      ],
-    });
-  };
 
   addContent = (selector, editorContent) => {
     if ($(selector)) {
@@ -62,6 +20,13 @@ $(document).ready(function () {
   const editor = new Editor();
   editor.init(unescape(briefingContent), unescape(paragraphsContent));
   const btn = document.getElementById('btn_share');
-  if (btn) editor.KaKaoShare();
-  console.log(btn.id);
+  const pathname = window.location.pathname;
+  const article = {
+    path: pathname.substring(1, pathname.length),
+    headline: document.title,
+    briefing: briefingContent,
+    image: document.getElementsByTagName('img')[0].src
+  }
+  alert(article.image)
+  if (btn) KaKaoShare(btn.id, article);
 });
