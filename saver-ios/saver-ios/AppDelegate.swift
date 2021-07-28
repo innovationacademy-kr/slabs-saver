c0e4d64303aa66b914122c15f9e56febc1e3b4ce
feldblume5263 <feldblume5263@gmail.com>
2021-07-28 18:04:10 +0900

    feat: #281 noti push, background 설정
    noti가 작동하도록, info.plist 수정 (certifiacate - push notification,
    	background 추가)

--- saver-ios/saver-ios/AppDelegate.swift
+++ saver-ios/saver-ios/AppDelegate.swift
@@ -130,7 +130,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
     print(userInfo)
 
     // Change this to your preferred presentation option
-    completionHandler([[.alert, .sound]])
+    completionHandler([[.badge, .sound]])
   }
 
   func userNotificationCenter(_ center: UNUserNotificationCenter,
