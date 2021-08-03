5afaee2f2ad19651eedaf06a01232381617408fe
hokim <teamgoodaloe@gmail.com>
2021-08-03 09:39:53 +0900

    fix: devleop 머지에서 삭제된 라우터 복구 - devleop과 머지 과정에서 실수로 삭제된 checkBookmark 라우터 복구

--- saver-web/routes/bookmark.js
+++ saver-web/routes/bookmark.js
@@ -7,7 +7,7 @@ const bookmarkCtrl = require('../controllers/bookmark/bookmarkController');
 // 북마크
 
 router.get('/article', authMiddleware, bookmarkCtrl.request.getBookmark);
-
+router.get('/:id', authMiddleware, bookmarkCtrl.request.checkBookmark);
 router.post('/:id', authMiddleware, bookmarkCtrl.request.createBookmark);
 router.delete('/:id', authMiddleware, bookmarkCtrl.request.deleteBookmark);
 
