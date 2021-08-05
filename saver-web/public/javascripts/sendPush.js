d7e0f33f5cc5c9da9d190a2592b19ea110de1f9d
hokim <teamgoodaloe@gmail.com>
2021-08-05 10:46:01 +0900

    feat: 푸시알림 입력 폼 추가 - 폼을 통해 알림을 보내는 부분 추가

--- saver-web/public/javascripts/sendPush.js
+++ saver-web/public/javascripts/sendPush.js
@@ -1,4 +1,4 @@
-async function sendPush(articleId) {
+async function sendAriclePush(articleId) {
   const token = localStorage['jwtToken'];
 
   $.ajax({
@@ -7,9 +7,35 @@ async function sendPush(articleId) {
     headers: {
       'x-access-token': token,
     },
-    success: function () {},
+    success: function () {
+      alert('알림을 보냈습니다.');
+    },
     error: function (err) {
       console.log(err);
+      alert('알림을 보내지 못했습니다.');
+    },
+  });
+}
+
+async function sendMessagePush(messageData) {
+  const token = localStorage['jwtToken'];
+  const message = {};
+  message['message'] = messageData;
+  $.ajax({
+    url: `/firebase`,
+    type: 'post',
+    headers: {
+      'x-access-token': token,
+    },
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    data: JSON.stringify(message),
+    success: function () {
+      alert('알림을 보냈습니다.');
+    },
+    error: function (err) {
+      console.log(err);
+      alert('알림을 보내지 못했습니다.');
     },
   });
 }
