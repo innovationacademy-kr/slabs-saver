60bf9ae60b6060f058280cd354f0ebdd86c164ae
hokim <teamgoodaloe@gmail.com>
2021-08-06 19:26:12 +0900

    refactor: 필요없는 부분 삭제 -필요 없는 부분 삭제

--- saver-web/public/javascripts/sendPush.js
+++ saver-web/public/javascripts/sendPush.js
@@ -1,22 +1,3 @@
-async function sendAriclePush(articleId) {
-  const token = localStorage['jwtToken'];
-
-  $.ajax({
-    url: `/author/push/${articleId}`,
-    type: 'post',
-    headers: {
-      'x-access-token': token,
-    },
-    success: function () {
-      alert('알림을 보냈습니다.');
-      
-    },
-    error: function (err) {
-      console.log(err);
-      alert('알림을 보내지 못했습니다.');
-    },
-  });
-}
 
 async function sendMessagePush(messageData) {
   const token = localStorage['jwtToken'];
