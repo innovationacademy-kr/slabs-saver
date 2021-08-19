e9b92adf97aaf837283b3b234baab6074b88a4cd
JunHyun Park <62925498+parkjh2568@users.noreply.github.com>
2021-08-19 14:11:54 +0900

    Revert "[feat] #435 설정페이지 프로토타입 제작, 로그아웃, 알람끄기"

--- saver-web/app.js
+++ saver-web/app.js
@@ -11,7 +11,6 @@ const app = express();
 
 sequelize.sync();
 // view engine setup
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -31,8 +30,6 @@ const sectionRouter = require('./routes/section');
 const todayRouter = require('./routes/today');
 const bookmarkRouter = require('./routes/bookmark');
 const alarmRouter = require('./routes/alarm');
-const settingRouter = require('./routes/setting');
-
 
 const layout = require('express-ejs-layouts');
 app.use(layout);
@@ -53,7 +50,6 @@ app.use('/subscriber', subscriberRouter);
 app.use('/today', todayRouter);
 app.use('/bookmark', bookmarkRouter);
 app.use('/alarm', alarmRouter);
-app.use('/setting', settingRouter);
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
--- saver-web/public/javascripts/isAlive.js
+++
@@ -1,6 +0,0 @@
-var jwt = require('jsonwebtoken');
-const token = localStorage['jwtToken'];
-
-console.log("-----------------------------------------");
-console.log(jwt.verify(token, SECRET));
-console.log("-----------------------------------------");
