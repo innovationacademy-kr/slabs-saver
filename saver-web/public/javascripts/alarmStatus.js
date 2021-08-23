85e196bab4837364fed8eee76910f29ac665f8b1
hokim <teamgoodaloe@gmail.com>
2021-08-23 18:56:07 +0900

    feat: 토큰이 유효하지 않을 경우 로그인페이지로 리다이렉트

--- saver-web/public/javascripts/alarmStatus.js
+++ saver-web/public/javascripts/alarmStatus.js
@@ -10,6 +10,18 @@ const getAlarmStatus = () => {
 		}).then((res) => {
 			if (res.data.alarmStatus === 2) alarmIcon.classList.add('new');
 		}).catch(error => {
+			if(error.response.status === 403)
+			{
+				var url = location.href
+				url = url.replace("/logined","")
+				url = url.substring(url.lastIndexOf('/'),url.length)
+			
+				localStorage.removeItem('jwtToken')
+				if(url == '/')
+					location.href = `/`;
+				else
+			 		location.href = `/subscriber/login?redirect=${url}`;
+			} 
 			console.log(error.response.data.message);
 		});
 	}
