65bc7ffce755c0ced9a66c819ca098e61584b6a1
kyu-baek <69143394+kyu-baek@users.noreply.github.com>
2021-08-23 19:37:05 +0900

    feat: #502 토큰값 .env 로 옮기기 1. auth.js, subdcriberController.js 파일에 있던 토큰값을,  .env 경로에 선언되어 있는 변수에서 찾을 수 있도록 변경

--- saver-web/middlewares/auth.js
+++ saver-web/middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-var tokenKey = 'slab!#abcd';
+var tokenKey = process.env.JWT_SECRET;
 const authMiddleware = (req, res, next) => {
   const token = req.headers['x-access-token'] || req.query.token;
   if (!token) {
