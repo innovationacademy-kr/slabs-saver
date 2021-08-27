46ffb707db9b47221d2731768393e4bba12536d1
hokim <teamgoodaloe@gmail.com>
2021-08-27 13:40:35 +0900

    feat: 비로그인 상태의 설정창 작성 -비로그인 상태에서도 고객센터나 개인정보 처리방침을 볼 수 있도록 수정

--- saver-web/routes/setting.js
+++ saver-web/routes/setting.js
@@ -4,7 +4,8 @@ const authMiddleware = require('../middlewares/auth');
 const settingCtrl = require('../controllers/user/settingController');
 
 // 알림페이지 연결
-router.get('/', settingCtrl.section);
+router.get('/', (req,res)=>{res.render('user/setting', { title: 'slab-saver', layout: 'layout/userLayout' })});
+router.get('/login', settingCtrl.section);
 router.get('/getuser', authMiddleware, settingCtrl.request.getUser);
 router.post('/alarmOnOff/:flag', authMiddleware, settingCtrl.request.OnOff);
 
